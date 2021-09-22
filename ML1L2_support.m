2717d69fc8d8afe94e23dfa163400dec1dacdfbb
boconn7782 <b.oconnell@northeastern.edu>
2021-09-22 16:16:33 -0400

    Update ML1L2_support.m

--- ML1L2_support.m
+++ ML1L2_support.m
@@ -1,7 +1,7 @@
 % MATLAB Lab #1 Part 2 Support
 % B.O'Connell
 % 2021-09-15
-% Weather Data Downloader 
+% Weather Downloader 
 % This program will provide N days of weather data
 % from a NOAA weather station, specifically the 
 % Mt. Washington weather station based on a 
@@ -30,13 +30,14 @@ N = input('Enter number of days: ');
 
 % Prepare and format all information as required to use
 % the GHCN (Global Historical Climatology Network)
+% https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt
 dataset =  'daily-summaries';
 station = 'USW00014755'; % Mt. Washington station ID
 % For more information on this weather station:
 % https://www.ncdc.noaa.gov/cdo-web/datasets/GHCND/stations/GHCND:USW00014755/detail
 startDate = char(datetime(D,'InputFormat','yyyy-MM-dd','Format','y-MM-dd'));
 endDate =   char(datetime(D,'InputFormat','yyyy-MM-dd','Format','y-MM-dd')+(N-1));
-dataTypes = 'TMAX,TMIN,AWND';
+dataTypes = 'TMAX,TMIN,AWND,FMTM,TAVG';
 format =    'csv'; 
 base = 'https://www.ncei.noaa.gov/access/services/data/v1?dataset=';
 call = strcat(base,dataset,'&dataTypes=',dataTypes,...
@@ -47,13 +48,17 @@ Data = webread(call);
 
 % Break the data into usable arrays
 DT = Data.('DATE'); 
-WD = Data.('AWND');
-LT = Data.('TMIN');
-HT = Data.('TMAX');
+% Wind provided in 10ths of meters per second
+WD = (Data.('AWND')*.1); % Converted to MPS
+% Temperature provided 10ths of degrees C
+LT = (Data.('TMIN')*.1); % Converted to deg C
+HT = (Data.('TMAX')*.1); % Converted to deg C
+TA = (Data.('TAVG')*.1); % Converted to deg C
 
 % plot data (poorly)
-plot(DT, WD, DT, LT, DT, HT);
+plot(DT, WD, DT, LT, DT, HT, DT, TA);
 legend('Winds','Low','High');
+
 
 
 
