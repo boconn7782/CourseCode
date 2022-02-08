2da2e092efd512904f807944c2360cbff6726a06
boconn7782 <b.oconnell@northeastern.edu>
2022-02-08 11:50:58 -0500

    Create ML2L1_Support.m

---
+++ ML2L1_Support.m
@@ -0,0 +1,25 @@
+% MATLAB Lab #2 Part 1
+% <first Initial>.<Last Name>
+% <Date of creation>
+% <Name of program>
+% <Description of program>
+
+clear;  % Clears the command window
+clc;    % Clears the workspace
+close;  % Closes any figures
+
+% Provide user info. and instruction
+disp(' - - - Grade Converter - - - ');
+disp('This program converts ... ');
+disp('When prompted, enter ... ');
+% Ask user for the final grade as a percentage
+% Compare that numeric grade to the conversion rules
+% IF percentage is 90.0% or above THEN
+%   Letter grade is an A
+% ELSEIF percentage is 80.0% to 90.0% THEN
+%   Letter grade is a B
+< Continue ELSEIFs for all grading rules >
+% ENDIF
+% Return the letter grade to the user
+
+
