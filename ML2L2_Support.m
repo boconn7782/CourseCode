222664c6000de5511a174f550376dfc917de226f
boconn7782 <b.oconnell@northeastern.edu>
2022-02-08 12:39:19 -0500

    Update ML2L2_Support.m

--- ML2L2_Support.m
+++ ML2L2_Support.m
@@ -11,8 +11,6 @@ close; % Closes any figures
 % Imports some compression data
 B = load('BridgeData.txt');
 % eliminate extraneous noise
-B(:,3)=smoothdata(B(:,2));
-plot(B(:,1),B(:,2),B(:,1),B(:,3));
 % Trim unnecessary information
 % Identify performance metrics
 % Plot the updated data
