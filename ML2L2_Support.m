cfa3753f1933f113af2fc6eb148ad2ea9d7ac1d5
boconn7782 <b.oconnell@northeastern.edu>
2022-02-11 10:58:51 -0500

    Update ML2L2_Support.m

--- ML2L2_Support.m
+++ ML2L2_Support.m
@@ -13,9 +13,9 @@ B = load('BridgeData.txt');
 plot(B(:,1),B(:,2)
 
 %% eliminate extraneous noise
-B(:,3) = smoothdata(B(:,2)); % Creates a force column
-% Plot position vs raw force( Col 1 vs col 2) 
-% and Position vs calculated load (col 1 vs col 3)
+[R,C] = size(B);
+B(:,3) = smoothdata(B(:,2),'Gaussian',.03*R); % Creates a force column
+% Plot position vs raw force( Col 1 vs col 2) and Position vs calculated load (col 1 vs col 3)
 % plot(B(:,1),B(:,2), B(:,1),B(:,3));
 
 %% Trim unnecessary information
@@ -38,8 +38,12 @@ B(:,3) = smoothdata(B(:,2)); % Creates a force column
 % < M = Strength (Max Load) >
 % < Deflection @ Max Load >
 % < Deflection @ Break >
-% < Toughness >
-
+% < Toughness - Perform a Reimann's Sum>
+%   Add 4th column to dataset to track Change in Energy 
+%   FOR each row(i) in dataset
+%       Change in energy(i) = (Defl. - Last Defl.)((Load + Last Load)/2)
+%   ENDFOR
+%   Toughness = Sum of the Change in Energy column
 
 %% Plot the updated data
 
