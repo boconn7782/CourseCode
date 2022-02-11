f1939f66b5db39a71554845c1701d6b00929b5e3
boconn7782 <b.oconnell@northeastern.edu>
2022-02-11 13:34:45 -0500

    Update ML2L2_Support.m

--- ML2L2_Support.m
+++ ML2L2_Support.m
@@ -10,13 +10,13 @@ close; % Closes any figures
 
 %% Imports some compression data
 B = load('BridgeData.txt');
-plot(B(:,1),B(:,2))
+
 
 %% eliminate extraneous noise
 [R,C] = size(B);
 B(:,3) = smoothdata(B(:,2),'Gaussian',.03*R); % Creates a force column
 % Plot position vs raw force( Col 1 vs col 2) and Position vs calculated load (col 1 vs col 3)
-% plot(B(:,1),B(:,2), B(:,1),B(:,3));
+plot(B(:,1),B(:,2), B(:,1),B(:,3));
 
 %% Trim unnecessary information
 % < Determine the Max load >
