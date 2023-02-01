ad93133ef78aba8629b33acb3f3c4a6ccb51227f
boconn7782 <b.oconnell@northeastern.edu>
2023-02-01 11:49:12 -0500

    Create ML2H1_Support.m

---
+++ MATLAB/ML2H1_Support.m
@@ -0,0 +1,27 @@
+% MATLAB Lab #2 Homework Part 1
+% <first Initial>.<Last Name>
+% <Date of creation>
+% <Name of program>
+% <Description of program>
+
+clear;  % Clears the command window
+clc;    % Clears the workspace
+close;  % Closes any figures
+
+% Imports some compression data
+
+
+% eliminate extraneous noise
+
+
+% Trim unnecessary information
+
+
+% Identify performance metrics
+
+
+% Plot the updated data
+
+
+% Print performance metrics
+
