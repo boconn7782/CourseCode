53fdd7f3abadb66834b2c63318f1383ea269c9bf
boconn7782 <b.oconnell@northeastern.edu>
2024-02-12 09:31:54 -0500

    Update P5H1_support.m
    Update some of the instructional text for added clarity

--- MATLAB/P5H1_support.m
+++ MATLAB/P5H1_support.m
@@ -12,28 +12,24 @@ clc;    % Clears the workspace
 close;  % Closes any figures
 
 
-
-
 % Introduction telling the user what the script does and what to do
 fprintf(...
     ['This program will provide weather data from\n'...
     ,'a NOAA weather station based on a user inputted\n'...
-    ,'date and number of days.'...
-    ,'Enter the date in the format of\n'...
-    ,'year-month-day (ex: 1990-06-01 ).\n']);
-% This could have been done using disp() but
-% using arrays of text [] and MATLAB's line break (...)
-% with fprintf is a good technique for printing a lot 
-% of text to the command window and splitting it 
-% across multiple lines.
+    ,'number of days and a start date of 2007-08-01.'...
+    ,'Enter the number of days when prompted.\n']);
+% This could have been done using disp(), BUT
+% I did it using arrays of text [] and MATLAB's code line break (...), 
+% which allows code to continue on a new line, with fprintf to 
+% showcase a good technique for printing a lot of formatted text to 
+% the command window while splitting the actual script across multiple lines
+% to make it easier for the programmer to read and interpret. 
 
 
 
-
-% User inputs for the date and number of days of data
-D = input('Enter a date: ','s');
+% Establish Time period based on start date and User input for # of days
+D = '2007-08-01'; % Date must be in year-month-day and formatted as a character array
 N = input('Enter number of days: ');
-
 
 
 
@@ -58,7 +54,6 @@ Data = webread(call);
 
 
 
-
 % Break out the data needed: 
 % Dates(DT), wind speeds(WD), low temperatures(LT), and high temperatures(HT)
 DT = Data.('DATE'); 
@@ -67,7 +62,6 @@ WD = (Data.('AWND')*.1)*(1000/(60*60)); % Converted to kph
 % Temperature provided 10ths of degrees C
 LT = (Data.('TMIN')*.1); % Converted to deg C
 HT = (Data.('TMAX')*.1); % Converted to deg C
-
 
 
 
