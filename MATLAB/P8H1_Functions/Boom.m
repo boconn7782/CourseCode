03e724f94b268fec6ad1dead9197c985c3bdbd0d
boconn7782 <b.oconnell@northeastern.edu>
2024-03-21 16:40:33 -0400

    Naming convention update
    Update to reflect the new naming convention

--- MATLAB/P8H1_Functions/Boom.m
+++ MATLAB/P8H1_Functions/Boom.m
@@ -1,6 +1,4 @@
 function Boom
-% B.O'Connell 2020-10-07
-% P/O ML3LP1
 % This function displays a ASCII message for a Minefield game. 
 disp('   ____     U  ___ u   U  ___ u  __  __    _     _    ')
 disp('U | __")u    \/"_ \/    \/"_ \/U|. \/ .|uU|"|u U|"|u  ')
